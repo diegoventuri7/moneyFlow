ed0d12d91ec1eea683b26b67186a487869ba8a80
Diego Venturi <diego.venturi@objective.com.br>
2021-05-20 09:15:58 -0300

    test: integration test

--- test/integration/api/transactions/transactions-create.test.js
+++ test/integration/api/transactions/transactions-create.test.js
@@ -1,9 +1,11 @@
 const chai = require('chai')
-const chaiHttp = require('chai-http')
+chai.use(require('chai-http'))
+chai.use(require('chai-datetime'))
+const expect = chai.expect
+const _ = require('lodash')
 const Server = require('../../../../src/loaders/server.js')
 const DatabaseMock = require('../../../utils/database-mock.js')
-const expect = chai.expect
-chai.use(chaiHttp)
+const { checkTransactionDatabase } = require('./transactions-support-test.js')
 
 describe('Endpoint-transactions-create', function () {
   this.timeout(5000)
@@ -17,56 +19,103 @@ describe('Endpoint-transactions-create', function () {
     app = server.getApp()
   })
 
-  it('Happy day: Create a transaction', function (done) {
-    const body = {
-      type: 'INCOME',
-      description: 'Salário',
-      amount: 1815,
-      dueDate: '05-02-2021',
-      category: 'OTHERS',
-      status: 'PENDING',
-      paymentDate: '05-02-2021',
-      paymentMethod: 'ITAU',
-      recurringTransactionId: null
-    }
-
-    chai.request(app)
-      .post('/api/transactions')
-      .send(body)
-      .end(function (err, res) {
-        expect(err).is.equal(null)
-        expect(res).to.have.status(201)
-        console.log(res.body)
-        done()
-      })
-  })
-
-  // it('Happy day: Get a ticker', function (done) {
-  //   chai.request(app)
-  //     .get('/api/tickers')
-  //     .query({ ticker: TICKERS[0].ticker })
-  //     .end(function (err, res) {
-  //       expect(err).is.equal(null)
-  //       expect(res).to.have.status(200)
-  //       console.log(JSON.stringify(res, null, 3))
-  //       done()
-  //     })
-  // })
-
-  // it('Happy day: Quote a ticker', function (done) {
-  //   chai.request(app)
-  //     .get('/api/tickers/quote')
-  //     .query({ tickers: 'IAU' })
-  //     .end(function (err, res) {
-  //       expect(err).is.equal(null)
-  //       expect(res).to.have.status(200)
-  //       const body = JSON.parse(res.text)
-  //       console.log(JSON.stringify(body, null, 3))
-  //       done()
-  //     })
-  // })
-
   after('Close server', async function () {
     await server.stop()
   })
+
+  it('Happy day: Create a spot income', async function () {
+    const body = {
+      type: 'INCOME',
+      description: 'loan',
+      amount: 26,
+      date: '05-13-2021',
+      category: 'OTHERS',
+      status: 'PENDING',
+      method: 'ITAU'
+    }
+
+    const res = await chai.request(app).post('/api/transactions').send(body)
+    expect(res).to.have.status(201)
+    expect(res.body).to.be.an('array').to.have.lengthOf(1)
+    await checkTransactionResponse(res.body[0], body)
+  })
+
+  it('Happy day: Create a spot expense', async function () {
+    const body = {
+      type: 'EXPENSE',
+      description: 'Energy bill',
+      amount: 199.65,
+      date: '09-25-2021',
+      category: 'HOME',
+      status: 'DONE',
+      method: 'MERCADO PAGO'
+    }
+
+    const res = await chai.request(app).post('/api/transactions').send(body)
+    expect(res).to.have.status(201)
+    expect(res.body).to.be.an('array').to.have.lengthOf(1)
+    await checkTransactionResponse(res.body[0], body)
+  })
+
+  it('Happy day: Create a installment income', async function () {
+    const body = {
+      type: 'INCOME',
+      description: 'loan 3x',
+      amount: 100,
+      date: '06-18-2021',
+      category: 'SERVICES',
+      status: 'PENDING',
+      method: 'NUBANK',
+      numberOfInstallments: 3,
+      installmentsPeriod: 'months'
+    }
+
+    const res = await chai.request(app).post('/api/transactions').send(body)
+    expect(res).to.have.status(201)
+    expect(res.body).to.be.an('array').to.have.lengthOf(3)
+    expect(Object.values(_.countBy(res.body, 'installmentsId'))[0]).to.equal(3)
+    await checkTransactionResponse(res.body[0], { ...body, description: 'loan 3x (1/3)', amount: 33.34, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[1], { ...body, date: '07-18-2021', description: 'loan 3x (2/3)', amount: 33.33, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[2], { ...body, date: '08-18-2021', description: 'loan 3x (3/3)', amount: 33.33, installmentsTotal: body.amount })
+  })
+
+  it('Happy day: Create a installment expense', async function () {
+    const body = {
+      type: 'EXPENSE',
+      description: 'Fly Tickets',
+      amount: 4444,
+      date: '11-05-2021',
+      category: 'OTHERS',
+      status: 'PENDING',
+      method: 'ITAU',
+      numberOfInstallments: 5,
+      installmentsPeriod: 'months'
+    }
+
+    const res = await chai.request(app).post('/api/transactions').send(body)
+    expect(res).to.have.status(201)
+    expect(res.body).to.be.an('array').to.have.lengthOf(5)
+    expect(Object.values(_.countBy(res.body, 'installmentsId'))[0]).to.equal(5)
+    await checkTransactionResponse(res.body[0], { ...body, description: 'Fly Tickets (1/5)', amount: 888.80, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[1], { ...body, date: '12-05-2021', description: 'Fly Tickets (2/5)', amount: 888.80, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[2], { ...body, date: '01-05-2022', description: 'Fly Tickets (3/5)', amount: 888.80, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[3], { ...body, date: '02-05-2022', description: 'Fly Tickets (4/5)', amount: 888.80, installmentsTotal: body.amount })
+    await checkTransactionResponse(res.body[4], { ...body, date: '03-05-2022', description: 'Fly Tickets (5/5)', amount: 888.80, installmentsTotal: body.amount })
+  })
+
+  async function checkTransactionResponse (response, check) {
+    expect(response.type).is.equal(check.type)
+    expect(response.description).is.equal(check.description)
+    expect(response.amount).is.equal(check.amount)
+    expect(new Date(response.date)).to.equalDate(new Date(check.date))
+    expect(response.category).is.equal(check.category)
+    expect(response.status).is.equal(check.status)
+    expect(response.method).is.equal(check.method)
+
+    if (check.numberOfInstallments > 0) {
+      expect(response.numberOfInstallments).is.equal(check.numberOfInstallments)
+      expect(response.installmentsTotal).is.equal(check.installmentsTotal)
+    }
+    await checkTransactionDatabase(response)
+  }
 })
--- test/integration/api/transactions/transactions-support-test.js
+++ test/integration/api/transactions/transactions-support-test.js
@@ -1,7 +1,25 @@
+const chai = require('chai')
+chai.use(require('chai-datetime'))
+const expect = chai.expect
+
+const transactionsRepository = require('../../../../src/api/repositories/transactions-repository.js')
+
 function createGenericTransaction (base = {}) {
   const transaction = {
 
   }
 }
 
-module.exports = { createGenericTransaction }
+async function checkTransactionDatabase (transaction) {
+  const base = await transactionsRepository.getById(transaction._id)
+  expect(base).to.be.a('object')
+  expect(base.type).is.equal(transaction.type)
+  expect(base.description).is.equal(transaction.description)
+  expect(base.amount).is.equal(transaction.amount)
+  expect(base.date).to.equalDate(new Date(transaction.date))
+  expect(base.category).is.equal(transaction.category)
+  expect(base.status).is.equal(transaction.status)
+  expect(base.method).is.equal(transaction.method)
+}
+
+module.exports = { createGenericTransaction, checkTransactionDatabase }
